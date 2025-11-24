38b9a14cd6a1117e94c55b7b1ba3806a3837a952
Julian Tempelsman <11447520+jtemps@users.noreply.github.com>
2025-11-24 11:08:36 -0800

    Update Slack webhook URLs
    Updated Slack webhook URLs for production and development.

--- wheel.js
+++ wheel.js
@@ -14,8 +14,8 @@ const itemsListEl = document.getElementById("itemsList");
 const confettiEl = document.getElementById("confetti");
 
 // Slack webhook URL
-const SF_TEAMS_SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/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";
-const DEV_SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/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";
+const SF_TEAMS_SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/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";
+const DEV_SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/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";
 const DEBUG = false;
 const CHANNEL_TO_USE = DEBUG ? DEV_SLACK_WEBHOOK_URL : SF_TEAMS_SLACK_WEBHOOK_URL;
 // Spin history storage
